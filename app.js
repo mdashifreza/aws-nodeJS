2bd0232d30b4219dfb170895900a13f5969ad468
mdashif5@gmail.com <55871288+mdashifreza@users.noreply.github.com>
2023-04-16 16:13:01 +0530

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req,res)=>{
     res.statusCode = 200;
     res.setHeader('Content-Type','text/plain');
-    res.end("heelo aws program");
+    res.end("hello nodeJS application on aws server deployed");
 })
 const port = process.env.port || 3001;
 server.listen(port,()=>{
